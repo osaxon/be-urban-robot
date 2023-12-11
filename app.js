1a089f884aa2cc3f2d7a32ddbdb3411679f15833
Oliver Saxon <oliverrsaxon@gmail.com>
2023-12-11 09:32:32 +0000

    adds cors package to allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const {
     customErrorHandler,
     psqlErrorHandler,
@@ -13,6 +14,7 @@ const {
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/api", getEndpoints);
 
